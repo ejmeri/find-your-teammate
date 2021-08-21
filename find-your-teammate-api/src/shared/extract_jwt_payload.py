001ebd7809cbbe5a7a6e5198b5269cf7f902f3bd
ejmeri <elmerisilva@hotmail.com>
2021-08-21 20:30:38 -0300

    class to extract jwt paylo from authorization

---
+++ find-your-teammate-api/src/shared/extract_jwt_payload.py
@@ -0,0 +1,22 @@
+
+
+from src.shared.api_return import ApiReturn
+from src.business.users.entity.user_auth import UserAuth
+
+class ExtractJwtPayload: 
+    @staticmethod
+    def extract(auth_header):
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+
+        try:
+            userAuth = UserAuth.decode_auth_token(auth_token)
+        except RuntimeError as error:
+            return ApiReturn.error('Acesso negado', str(error)), 401            
+
+        if not userAuth['user_id']:
+            return ApiReturn.error('Erro na sessão logada, por favor, faça login novamente'), 400
+
+        return userAuth

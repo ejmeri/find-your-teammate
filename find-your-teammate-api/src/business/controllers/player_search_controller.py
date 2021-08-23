e19f2ad7cd5343ee9745903f1858f4d4116d3de4
ejmeri <elmerisilva@hotmail.com>
2021-08-23 01:07:40 -0300

    api - visit profile player

--- find-your-teammate-api/src/business/controllers/player_search_controller.py
+++ find-your-teammate-api/src/business/controllers/player_search_controller.py
@@ -1,10 +1,12 @@
-from src.business.players.entity.player_info import Player
-from src.business.players.control.player_repository import PlayerRepository
 from flask import request
+from bson import ObjectId
 from flask.blueprints import Blueprint
 
 from src.shared.api_return import ApiReturn
 from src.shared.extract_jwt_payload import ExtractJwtPayload
+from src.business.players.entity.player_info import Player
+
+from src.business.players.control.player_repository import PlayerRepository
 
 playerSearchController = Blueprint(
     'player_searchs_controller', __name__, url_prefix='/players/search')
@@ -21,7 +23,7 @@ class PlayerSearchController:
         rankFilter = request.args.get('rank')
 
         playersDb = PlayerRepository.findPlayers(rankFilter)
-        players: Player =  []
+        players: Player = []
 
         for player in playersDb:
             player['_id'] = str(player['_id'])
@@ -29,3 +31,20 @@ class PlayerSearchController:
             players.append(player)
 
         return ApiReturn.success(response=players), 200
+
+    @playerSearchController.get('<id>')
+    def visitProfile(id: str):
+        if not ObjectId.is_valid(id):
+            return ApiReturn.error('Id inválido'), 400
+
+        auth_user = ExtractJwtPayload.extract(
+            request.headers.get('Authorization'))
+        if 'internalError' in auth_user.keys():
+            return auth_user, 401
+
+        player = PlayerRepository.findByUserId(id)
+        if player:
+            player['_id'] = str(player['_id'])
+            player['userId'] = str(player['userId'])
+
+        return ApiReturn.success(response=player), 200
